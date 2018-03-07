3835b7a23190b15c9d8f272cdc8c1121f2a8c1fd
Lucy Follansbee <lfollansbee@gmail.com>
2018-03-06 18:18:22 -0700

    complete section 12 - pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -5,15 +5,58 @@
 // pyramid has spaces on both the left *and* right hand sides
 // --- Examples
 //   pyramid(1)
-//       '#'
+//       '#' n
 //   pyramid(2)
 //       ' # '
-//       '###'
+//       '###' n + 1
 //   pyramid(3)
 //       '  #  '
 //       ' ### '
-//       '#####'
+//       '#####' n + 2
+//   pyramid(4)
+//       '   #   '
+//       '  ###  '
+//       ' ##### '
+//       '#######' n + 3
 
-function pyramid(n) {}
+// function pyramid(n) {
+//   const rowLength = n + (n - 1)
+//
+//   for (let row = 0; row < n; row ++) {
+//     let level = ""
+//     let spaces = n - (row + 1)
+//
+//     for (let col = 0; col < rowLength; col++) {
+//       if (col < spaces || col >= rowLength - spaces) {
+//         level += " "
+//       } else {
+//         level += "#"
+//       }
+//     }
+//
+//     console.log(level)
+//   }
+// }
+
+function pyramid(n, row = 0, level = "") {
+    const rowLength = n + (n - 1)
+    const spaces = n - (row + 1)
+
+    if (row === n) {
+        return
+    }
+
+    if (level.length === rowLength) {
+        console.log(level)
+        return pyramid(n, row + 1)
+    }
+
+    if (level.length < spaces || level.length >= rowLength - spaces) {
+        level += " "
+    } else {
+        level += "#"
+    }
+    return pyramid(n, row, level)
+}
 
 module.exports = pyramid;
